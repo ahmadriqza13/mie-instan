4b4fbbb4e3f8f87f70fb5e3312be0a02f94b3e1c
ahmadriqza13 <aridho78526@gmail.com>
2024-12-05 12:22:47 +0700

    memasak mie instan

---
+++ memasak.php
@@ -0,0 +1,11 @@
+<?php
+echo "memasak mie instan";
+echo "-------------------";
+echo "ambil air menggunakan panci secukupnya";
+echo "lalu letakan panci dikompor rebus air hingga mendidih";
+echo "setelah mendidih masukan mie tunggu hingga matang";
+echo "selagi menunggu mie matang siapkan bumbu mie instan diatas piring";
+echo "jika mie sudah terlihat matang tiriskan mie dipiring yang sudah dibumbui";
+echo "lalu aduk mie hingga merata";
+echo "kemudian siap dimakan";
+?>
