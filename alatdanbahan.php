b597a9d9678b8f959589df09259c5e9da50e5629
Laras Agustin <larasaguntiin01@gmail.com>
2024-12-05 13:31:57 +0700

    alatdanbahan

---
+++ alatdanbahan.php
@@ -0,0 +1,14 @@
+<?php
+echo "Alat dan bahan";
+echo "----------------";
+echo "1. piring";
+echo "2. garpu";
+echo "3. sendok";
+echo "4. sendok";
+echo "5. panci";
+echo "6. air";
+echo "7. panci";
+echo "8. mie instan";
+echo "9. cabai";
+echo "10. sawi";
+?>
